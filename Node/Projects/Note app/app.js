5b0ed47f1b5861a90f8b30ceaa5beb229324cfe6
Mayur Jiyani <mayur.jiyani@marutitech.com>
2022-01-05 10:07:43 +0530

    initial commit

---
+++ Node/Projects/Note app/app.js
@@ -0,0 +1,8 @@
+// const fs = require('fs')
+
+// fs.writeFileSync('notes.txt', 'Here is the conetent of notes');
+// fs.appendFileSync('notes.txt', 'Your first note');
+
+const name = require('./utils')
+
+console.log;
