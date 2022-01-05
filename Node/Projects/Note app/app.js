a18c77debee980997cf38ea9119896318d05fd7d
Mayur Jiyani <mayur.jiyani@marutitech.com>
2022-01-05 11:13:18 +0530

    Added printing color

--- Node/Projects/Note app/app.js
+++ Node/Projects/Note app/app.js
@@ -1,5 +1,10 @@
-const note = require('./notes');
+// const note = require('./notes');
 
-const validate = require('validator');
-console.log(validate.isEmail('mayur@gmail.com'));
-console.log(validate.isURL('mayur.com'));
+// const validate = require('validator');
+// console.log(validate.isEmail('mayur@gmail.com'));
+// console.log(validate.isURL('mayur.com'));
+
+
+const chalk = require('chalk');
+
+console.log(chalk.green.bold.inverse('Success'));
