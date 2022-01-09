348f38e7b6e3584942d245c54afb008ecd74215f
mayur-jiyani <97055499+mayur-jiyani@users.noreply.github.com>
2022-01-09 16:04:26 +0530

    added search form

--- Node/Projects/web-server/src/app.js
+++ Node/Projects/web-server/src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App Homepage',
+        title: 'Weather',
         name: 'mayur jiyani'
     })
 })
