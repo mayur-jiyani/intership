d5325841d4b8596797b0c9ceee62ab1d5efc927a
mayur-jiyani <97055499+mayur-jiyani@users.noreply.github.com>
2022-01-13 14:34:33 +0530

    async-await added

---
+++ Node/Projects/playground/async-await.js
@@ -0,0 +1,23 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000);
+    })
+}
+
+
+const doWork = async () => {
+    const sum = await add(1, 2)
+    const sum1 = await add(1, sum)
+    const sum2 = await add(1, sum1)
+    const sum3 = await add(1, sum2)
+    return sum3
+
+}
+
+doWork().then((result) => {
+    console.log(result)
+}).catch((e) => {
+    console.log(e)
+})
