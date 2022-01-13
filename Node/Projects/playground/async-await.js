74ed50892dc622015cf25145392d947e1e4218a6
mayur-jiyani <97055499+mayur-jiyani@users.noreply.github.com>
2022-01-13 18:42:00 +0530

    Updated

--- Node/Projects/playground/async-await.js
+++ Node/Projects/playground/async-await.js
@@ -1,6 +1,9 @@
 const add = (a, b) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            if (a < 0 || b < 0) {
+                reject("number must be positive")
+            }
             resolve(a + b)
         }, 2000);
     })
@@ -8,7 +11,7 @@ const add = (a, b) => {
 
 
 const doWork = async () => {
-    const sum = await add(1, 2)
+    const sum = await add(-1, 2)
     const sum1 = await add(1, sum)
     const sum2 = await add(1, sum1)
     const sum3 = await add(1, sum2)
